1f6e5c50a2981355aeed9777c137bf75766b20ea
Tony Isidro <tonyi@Tonys-MacBook-Pro-3.local>
2018-07-28 11:12:27 -0400

    Initial styles commit

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -5,7 +5,7 @@ const SearchBar = () =>
 
 <div className="search-all">
         <form className="search-bar">
-        <h2>Find Your H<i className="far fa-xs fa-smile-beam"></i>ppy Place</h2> <br />
+        <h2>Find your happy place.</h2>
             <input className="search" type="search" placeholder="Enter a City, State, or Zipcode" />
             <button className="submit" type="submit"><i className="fas fa-search"></i></button>
         </form>
