0839a9bd230841335fd013c27a4215aa85843ccf
The1987 <mrchrishantis@gmail.com>
2018-07-30 21:05:10 -0400

    test

--- client/src/pages/Dashboard/Dashboard.js
+++
@@ -1,9 +0,0 @@
-// Adding Tours
-//// CSV Import / Export
-
-// Business Analytics
-//// CSV Import / Export
-/// Xero or QB Integration
-
-// Settings
-//// Tax Rate -> Option when "Adding Tours"
--- client/src/pages/Profile/Profile.js
+++
@@ -1,3 +0,0 @@
-// View Purchase History (GET)
-// Cancel  and Get Refund for Recently Booked Tour (DELETE and UPDATE)
-//
