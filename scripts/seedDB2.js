39f80fa1c88c8d9eb58c87a964cd5481edf01a8a
Tony Isidro <tonyi@Tonys-MBP-3.fios-router.home>
2018-08-04 21:19:22 -0400

    Changed database name

--- scripts/seedDB2.js
+++ scripts/seedDB2.js
@@ -5,7 +5,7 @@
 // // This file empties the Books collection and inserts the books below
 
 // mongoose.connect(
-//   process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist",
+//   process.env.MONGODB_URI || "mongodb://localhost/tourBookerList",
 //   {
 //     useMongoClient: true
 //   }
